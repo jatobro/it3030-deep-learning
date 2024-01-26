239f20710a071fdcb1837693ddda9ada0087d7a9
jatobro <jakob.tossebro@gmail.com>
2024-01-26 17:32:11 +0100

    style: 💄 black formatting and ignore

--- backprop/layer.py
+++ backprop/layer.py
@@ -1,16 +1,29 @@
 import numpy as np
 from functions import relu
 
+
 class Layer:
-    def __init__(self, layer_sizes=(5, 5), activation_function=relu, weight_deviation=0.01):
-        self.weights = np.random.randn(layer_sizes[0], layer_sizes[1]) * weight_deviation
+    def __init__(
+        self, layer_sizes=(5, 5), activation_function=relu, weight_deviation=0.01
+    ):
+
+        self.weights = (
+            np.random.randn(layer_sizes[0], layer_sizes[1]) * weight_deviation
+        )
         self.biases = np.zeros(layer_sizes[0])
         self.activation_function = activation_function
 
     def forward_pass(self, inputs):
         outputs = []
         for input in inputs:
-            outputs.append(list(map(self.activation_function, np.dot(self.weights.T, input) + self.biases)))
+            outputs.append(
+                list(
+                    map(
+                        self.activation_function,
+                        np.dot(self.weights.T, input) + self.biases,
+                    )
+                )
+            )
 
         return np.array(outputs)
 
