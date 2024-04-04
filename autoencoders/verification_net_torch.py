33fa342b0f844d29aa129ef175396ddf804fe66f
jatobro <jakob.tossebro@gmail.com>
2024-04-04 17:35:12 +0200

    loader

--- autoencoders/verification_net_torch.py
+++ autoencoders/verification_net_torch.py
@@ -1,7 +1,9 @@
-from torch import nn, optim
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 
-class ClassificationModel(nn.Module):
+class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
@@ -10,15 +12,15 @@ class ClassificationModel(nn.Module):
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.25),
+            nn.Dropout(p=0.5),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.25),
+            nn.Dropout(p=0.5),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Dropout(p=0.25),
+            nn.Dropout(p=0.5),
         )
 
         self.flatten = nn.Flatten()
@@ -38,10 +40,27 @@ class ClassificationModel(nn.Module):
 
 
 class VerificationNet:
-    def __init__(self):
-        self.model = ClassificationModel()
+    def __init__(
+        self, force_learn: bool = False, file_name: str = "./models/verification_model"
+    ):
+        self.force_relearn = force_learn
+        self.file_name = file_name
+
+        self.model = Classifier()
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.01)
+
+        self.done_training = self.load()
+
+    def load(self):
+        try:
+            self.model.load_state_dict(torch.load(f=self.file_name))
+            return True
+        except:
+            print(
+                "Could not read weights for verification_net from file. Must retrain..."
+            )
+            return False
 
 
 if __name__ == "__main__":
